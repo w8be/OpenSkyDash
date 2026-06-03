f179eb51f4e1bef99dabb27b5dfb419159e748dd
w8be <60948440+w8be@users.noreply.github.com>
2026-06-03 13:48:21 -0400

    changest to main.js

--- src/main.js
+++ src/main.js
@@ -11,24 +11,24 @@ const app = createApp(App)
 
 registerPlugins(app)
 
-// // 1. Catch Vue-specific template & component crashes
-// app.config.errorHandler = (err, vm, info) => {
-//   const errorMessage = `🚨 VUE ERROR:\n${err.message}\n\nStack:\n${err.stack}\n\nInfo: ${info}`
-//   alert(errorMessage)
-//   console.error(err)
-// }
 
-// // 2. Catch standard JavaScript runtime errors (syntax, typos, etc.)
-// window.onerror = function (message, source, lineno, colno, error) {
-//   const errorMessage = `💥 JS ERROR:\n${message}\n\nFile: ${source}\nLine: ${lineno}:${colno}`
-//   alert(errorMessage)
-//   return false
-// }
 
-// // 3. Catch failed API calls or unhandled async/promise promises
-// window.addEventListener('unhandledrejection', function (event) {
-//   const errorMessage = `⏳ PROMISE REJECTION:\n${event.reason}`
-//   alert(errorMessage)
-// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.mount('#app')
